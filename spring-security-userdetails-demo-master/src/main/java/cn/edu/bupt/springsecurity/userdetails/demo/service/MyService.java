875841d5d0f7f3951a86eb518bcd0d72594e028f
guguqiui <lvqiuyuan1101@163.com>
2023-12-31 15:26:04 +0800

    2

--- spring-security-userdetails-demo-master/src/main/java/cn/edu/bupt/springsecurity/userdetails/demo/service/MyService.java
+++ spring-security-userdetails-demo-master/src/main/java/cn/edu/bupt/springsecurity/userdetails/demo/service/MyService.java
@@ -3,9 +3,14 @@ package cn.edu.bupt.springsecurity.userdetails.demo.service;
 import org.springframework.http.ResponseEntity;
 import org.springframework.http.HttpMethod;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
+import org.springframework.http.HttpEntity;
 import org.springframework.web.client.RestTemplate;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.Collections;
 
 @Service
 public class MyService {
@@ -20,7 +25,29 @@ public class MyService {
     public String getResponseFromApi() {
         String url = "https://api.ai2u.link/ai/";
 
-        ResponseEntity<String> response = restTemplate.exchange(url, HttpMethod.GET, null, String.class);
+        // 创建请求头
+        HttpHeaders headers = new HttpHeaders();
+        headers.setContentType(MediaType.APPLICATION_JSON);
+        headers.setAccept(Collections.singletonList(MediaType.APPLICATION_JSON));
+
+        // 创建请求体
+        String requestBody = "{"
+                + "\"service\": \"OpenAI\","
+                + "\"interface_name\": \"chatGPT_1\","
+                + "\"model\": \"gpt-3.5-turbo\","
+                + "\"messages\": ["
+                + "{"
+                + "\"role\": \"user\","
+                + "\"content\": \"Your knowledge cutoff is until when?\""
+                + "}"
+                + "]"
+                + "}";
+
+        // 创建 HttpEntity 对象
+        HttpEntity<String> entity = new HttpEntity<>(requestBody, headers);
+
+        // 发送 POST 请求
+        ResponseEntity<String> response = restTemplate.exchange(url, HttpMethod.POST, entity, String.class);
 
         // 获取状态码
         HttpStatus statusCode = response.getStatusCode();
